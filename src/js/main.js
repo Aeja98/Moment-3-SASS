1e6dc878b86cf534583ac6a18b6578c2c92c222a
Angelica Adolfsson <angelicaadolfsson1gmail.com>
2026-02-23 14:01:53 +0100

    Made animation page with three animations. 1-hover & rotate 2-button that moves when clicked on & js to keep it inside its area 3-loader the pulses & spins the menu was already animated but kept as is

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
 "use strict";
 import "../scss/main.scss";
+
+//Animation-btn movement
+const playArea = document.getElementById("playArea");
+const animatedBtn = document.getElementById("animatedBtn");
+
+if (playArea && animatedBtn) {
+    animatedBtn.addEventListener("click", () => {
+        const areaRect = playArea.getBoundingClientRect();
+        const btnRect = animatedBtn.getBoundingClientRect();
+
+        const maxX = areaRect.width - btnRect.width;
+        const maxY = areaRect.height - btnRect.height;
+
+        const x = Math.floor(Math.random() * maxX);
+        const y = Math.floor(Math.random() * maxY);
+        
+        animatedBtn.style.transform = `translate(${x}px, ${y}px)`;
+    });
+}
